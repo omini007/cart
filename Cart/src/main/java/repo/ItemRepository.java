ae56b3a00af63fb90a4fd77e411e29a508a64583
prongzz <prongzz.nil@gmail.com>
2018-01-07 18:51:34 +0530

    Changed from CrudRepository to JpaRepository

--- Cart/src/main/java/repo/ItemRepository.java
+++ Cart/src/main/java/repo/ItemRepository.java
@@ -1,10 +1,10 @@
 package repo;
 
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ItemRepository extends CrudRepository<Item, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long> {
 
 }
