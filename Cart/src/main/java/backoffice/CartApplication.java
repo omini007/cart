6c67d186a83c2ee1f6443dee667a17b6fa4f9e0f
Omkar Nikam <omkar.nikam24@gmail.com>
2018-01-23 22:09:17 +0530

    Update CartApplication.java

--- Cart/src/main/java/backoffice/CartApplication.java
+++ Cart/src/main/java/backoffice/CartApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages={"com.cart.backoffice","Repo","RestApi"})
 public class CartApplication {
 	// Omkar
+	// Omkar 2
 	public static void main(String[] args) {
 		SpringApplication.run(CartApplication.class, args);
 	}
