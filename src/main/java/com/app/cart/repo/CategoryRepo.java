01093497fc815f517c02e6a6736d034351d3f855
prongzz <prongzz.nil@gmail.com>
2018-02-04 01:31:00 +0530

    Basic end points created for Offer entity, started working for Model Mapper for sending mapped DTO's to client side.

---
+++ src/main/java/com/app/cart/repo/CategoryRepo.java
@@ -0,0 +1,13 @@
+package com.app.cart.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.app.cart.entity.Category;
+
+@Repository
+public interface CategoryRepo extends JpaRepository<Category, Integer> {
+	
+	
+
+}
